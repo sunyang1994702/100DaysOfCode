f3e4800763d00f3883bad4c59b37154f413333d6
sunyang1994702 <40493383+sunyang1994702@users.noreply.github.com>
2021-02-18 16:46:14 +0900

    Update demo1.py

--- Day02/demo1.py
+++ Day02/demo1.py
@@ -4,17 +4,15 @@ from email.mime.text import MIMEText
 from email.header import Header
  
 
-from_email = "sunyang1994702@yahoo.co.jp"
-pwd = "" 
+from_email = "*****@yahoo.co.jp"
+pwd = "*****" 
 
-to_email = "ounann_521@yahoo.co.jp"
+to_email = "*****@yahoo.co.jp"
 smtp_server = "smtp.mail.yahoo.co.jp"
 
  
 # write text content you want send.
 message = MIMEText('Python email sending test...', 'plain', 'utf-8')
-message['From'] = Header("Mail System", 'utf-8')   # name a sender
-message['To'] =  Header("Test User", 'utf-8')        # name a receiver
  
 subject = 'Python SMTP mail'
 message['Subject'] = Header(subject, 'utf-8')
@@ -24,8 +22,6 @@ try:
     smtpObj = smtplib.SMTP(smtp_server, 587)
     smtpObj.login(from_email, pwd)
     smtpObj.sendmail(from_email, to_email, message.as_string())
-    smtpObj.quit()
     print("email sending successfully!")
 except smtplib.SMTPException:
     print("Error: can not send email")
-    print(smtplib.SMTPException)
