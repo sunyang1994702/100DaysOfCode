a250188386c4b931cf40797c0a16e7f23d0698d8
sunyang1994702 <40493383+sunyang1994702@users.noreply.github.com>
2021-02-23 11:52:49 +0900

    Update and rename demo2.py to demo1.py

--- Day06/demo1.py
+++ Day06/demo1.py
@@ -55,7 +55,7 @@ def get_one_page(url):
 
 
 def save_data(results):
-    with open("C:/Users/sunya/OneDrive/桌面/100DaysOfCode/Day06/Douban_review_500.csv", 'a+', newline='', encoding='utf-8-sig') as f:
+    with open("Douban_review_500.csv", 'a+', newline='', encoding='utf-8-sig') as f:
         f_csv = csv.writer(f)
         f_csv.writerows(results)
 
@@ -89,6 +89,5 @@ if __name__ == '__main__':
     }
 
     cookies = {
-        'cookie': 'bid=gcfyoWWiWJY; __utmc=30149280; __utmz=30149280.1613896443.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmz=223695111.1613896443.1.1.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); __utmc=223695111; __gads=ID=16f40c9c51ec6f38-2271effe1ec60002:T=1613896444:RT=1613896444:S=ALNI_MZtzdODWQ96vdeGl6QSiegiQOtWXA; trc_cookie_storage=taboola%2520global%253Auser-id%3D8f472d2d-bccd-4c89-977c-925932754229-tuct5d349b1; _vwo_uuid_v2=D3C960EACC1C5F6E07A9429106CAF3B96|d247d26b8d01296568a2856d36f93df0; dbcl2="154772613:RlHyYstmHIc"; ck=I-92; push_noty_num=0; push_doumail_num=0; __utmv=30149280.15477; loc-last-index-location-id="118254"; ll="118254"; _ga=GA1.2.292864210.1613896443; ap_v=0,6.0; __utma=30149280.292864210.1613896443.1613896443.1613999766.2; __utma=223695111.1046750037.1613896443.1613896443.1613999766.2; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1613999766%2C%22https%3A%2F%2Fwww.google.com%2F%22%5D; _pk_id.100001.4cf6=7378738c5e5949d2.1613896443.2.1614000525.1613899841'
-    }
+        'cookie': ''    }
     main()
